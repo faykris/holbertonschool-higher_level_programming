f813c47a065ad26b8bff6d4814c65f873cde4259
Cristian Pinzón <faykris28@gmail.com>
2021-04-28 18:15:25 -0500

    add other way fotr task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
+    lenarg = len(sys.argv) - 1
     if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(lenarg))
     elif len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+        print("{} argument:".format(lenarg))
         for i, arg in enumerate(sys.argv):
             if i != 0:
-                print(f"{i}: {arg}")
+                print("{}: {}".format(i, arg))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(lenarg))
         for i, arg in enumerate(sys.argv):
             if i != 0:
-                print(f"{i}: {arg}")
+                print("{}: {}".format(i, arg))
