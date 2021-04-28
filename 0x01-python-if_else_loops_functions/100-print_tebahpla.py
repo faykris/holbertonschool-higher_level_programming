1c97caf2bbd07ee80c8c5057c5850b6e252fc713
Cristian Pinzón <faykris28@gmail.com>
2021-04-28 11:11:24 -0500

    add code 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+c = 122
+t = 0
+while (c != 96):
+        if c % 2 != 0:
+                t = c
+                print("{:c}".format(t-32), end="")
+        else:
+                print("{:c}".format(c), end="")
+        c -= 1
