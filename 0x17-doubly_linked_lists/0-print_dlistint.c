17cadbb1a5822a35b6fff1ae19cf44243e3db4a9
Cristian Pinzón <faykris28@gmail.com>
2021-05-01 13:38:58 -0500

    add task 0 file 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (i);
+	
+	while (h != NULL)
+	{
+		if (h->prev == NULL && i != 0)
+			break;
+		if (h->next == NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->prev;
+		}
+		else
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+		i++;
+	}
+	return (i);
+}
