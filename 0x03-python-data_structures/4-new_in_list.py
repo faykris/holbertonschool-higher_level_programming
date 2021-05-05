5be180b5afabcb5a8387fda4a543652bcf1d2c19
Cristian Pinzón <faykris28@gmail.com>
2021-05-05 15:07:34 -0500

    Add task 2, 3, 4 files

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    i = 0
+    new_list = list(" " * len(my_list))
+
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    while i < len(my_list):
+        if i == idx:
+            new_list[i] = element
+        else:
+            new_list[i] = my_list[i]
+        i += 1
+    return new_list
