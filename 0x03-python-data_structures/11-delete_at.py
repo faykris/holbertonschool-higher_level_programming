6637b4896b2a487f7e62b7ad325764afdbab91e9
Cristian Pinzón <faykris28@gmail.com>
2021-05-06 09:51:47 -0500

    Update task 11 - fix with shortly way

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = list(" " * (len(my_list) - 1))
-    i = 0
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    elif idx == 0:
-        for ele in my_list:
-            if i != len(my_list) - 1:
-                new_list[i] = my_list[i + 1]
-            i += 1
-    elif idx == len(my_list) - 1:
-        for ele in my_list:
-            if i != len(my_list) - 1:
-                new_list[i] = my_list[i]
-            i += 1
-    else:
-        for ele in my_list:
-            if i != len(my_list) - 1:
-                new_list[i] = ele
-            i += 1
-    my_list = new_list
+    del my_list[idx]
     return my_list
