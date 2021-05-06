d7aa04814966bcd33bab5129ceb4a84aa97979bc
Cristian Pinzón <faykris28@gmail.com>
2021-05-05 20:25:14 -0500

    Update task 9 file 9-max_integer.py - fix empty list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    mint = my_list[0]
+
     if len(my_list) == 0:
         return "None"
+    else:
+        mint = my_list[0]
     for i in my_list:
         if i > mint:
             mint = i
