2233a484b05a1e66440eb079d7d63480827b44d9
Cristian Pinzón <faykris28@gmail.com>
2021-05-06 12:08:21 -0500

    Update task 13 file - add logical code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,5 +7,44 @@
  */
 int is_palindrome(listint_t **head)
 {
-	return (0);
+	listint_t *forward;
+	listint_t *backward;
+	listint_t *length;
+	int *list;
+	int i = 0, j = 0, k = 0;
+
+	if (!head)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	forward = *head;
+	backward = *head;
+	length = *head;
+	while (length != NULL)
+	{
+		length = length->next;
+		i++;
+	}
+	list = malloc(sizeof(int) * i);
+	while (backward != NULL)
+	{
+		list[j] = backward->n;
+		backward = backward->next;
+		j++;
+	}
+	j--;
+	while (forward != NULL)
+	{
+		if (k == j - k)
+			break;
+		else if (forward->n != list[j - k])
+		{
+			free(list);
+			return (0);
+		}
+		forward = forward->next;
+		k++;
+	}
+	free(list);
+	return (1);
 }
