3f50e6b25aa8733be994896e5fb29a2dfc81f213
Cristian Pinzón <faykris28@gmail.com>
2021-05-05 23:58:59 -0500

    Add last files task 13 empty, 11 with errors

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: address pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	return (0);
+}
