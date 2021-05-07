17fad7f5c184db8a18c1467573e6c1c335eb3dc1
Cristian Pinzón <faykris28@gmail.com>
2021-05-06 22:39:34 -0500

    Add task 12 file - add None and not str condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,10 @@ def roman_to_int(roman_string):
     r_nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     i = 0
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    if not roman_string:
+        return 0
     while i < len(roman_string):
         s1 = r_nums[roman_string[i]]
         if i + 1 < len(roman_string):
