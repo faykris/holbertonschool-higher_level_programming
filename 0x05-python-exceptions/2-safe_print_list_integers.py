6f1908ae2fb94e29c72d5f65d9a9fcd4fbb4618e
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 16:17:29 -0500

    Update 2-safe_print_list_integers.py - try with a zero index returning 0

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     printed = 0
     em_list = []
-    if my_list == em_list:
+    if my_list == em_list or x <= 0:
         print("")
         return 0
-    for i in range(x):
+    while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
@@ -14,6 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print(end="")
         except TypeError:
             print(end="")
+        i += 1
     if x > 0:
         print()
     return printed
