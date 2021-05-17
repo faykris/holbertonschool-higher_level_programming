b11f559a2ae41eca6cd0557526b22eabed33ca64
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 14:23:13 -0500

    Update 2-safe_print_list_integers.py - leave previous changes, change single by double quotes

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     while i < x:
         try:
-            print("{:d}".format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
         except ValueError:
             print(end="")
         except TypeError:
-            print(end="")
-        except IndexError:
             print(end="")
         i += 1
     if x > 0:
