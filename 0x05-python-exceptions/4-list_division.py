2e60cf79b0f3f08574fa86441f680ab75af5f057
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 12:20:09 -0500

    Modify 4-list_division.py - add return None in a empty lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     i = 0
+    if len(my_list_1) == 0 and len(my_list_1) == 0:
+        return None
     if len(my_list_1) > len(my_list_2):
         re_list = [0] * len(my_list_1)
     else:
