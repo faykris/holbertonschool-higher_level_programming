1a98c32a9d5e5de4af914cb87cbe24b43265b479
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 14:02:15 -0500

    Modify 0-safe_print_list.py - modify the logical to different way

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    if x <= 0 or my_list == []:
-        return i
     try:
         while i < x:
             print("{}".format(my_list[i]), end="")
             i += 1
+        print("")
+        return x
     except IndexError:
-        print(end="")
-    print("")
+        print("")
     return i
