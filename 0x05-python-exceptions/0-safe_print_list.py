3bd669441b2a41cd1dd189e7aa750e1e1d7731d3
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 13:23:26 -0500

    Modify 0-safe_print_list.py - modify the empty condition of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
+    if x <= 0 or len(my_list) == 0:
+        return 0
     try:
-        while i < x:  # for ele in my_list:
+        while i < x:
             print('{}'.format(my_list[i]), end='')
             i += 1
         if x > 0:
