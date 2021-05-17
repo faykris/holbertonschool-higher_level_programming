fd250d462b318c54b7b30a0d148e50ddba43473c
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 13:36:40 -0500

    Revert "Modify 4-list_division.py - try change format errors prints to {} .format()"
    This reverts commit 0513aaa695033a8f13c41aff8db83ade8837a7e3.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     i = 0
-    te = "wrong type"
-    ze = "division by 0"
-    ie = "out of range"
+    if len(my_list_2) == 0:
+        return my_list_1
     if len(my_list_1) > len(my_list_2):
         re_list = [0] * len(my_list_1)
     else:
@@ -12,13 +11,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             re_list[i] = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("{}".format(te))
+            print("wrong type")
             re_list[i] = 0
         except ZeroDivisionError:
-            print("{}".format(ze))
+            print("division by 0")
             re_list[i] = 0
         except IndexError:
-            print("{}".format(ie))
+            print("out of range")
             re_list[i] = 0
         finally:
             i += 1
