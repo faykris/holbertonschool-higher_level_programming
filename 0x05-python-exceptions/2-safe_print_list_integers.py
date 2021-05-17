e7c6a10590f93677d0bcbf8362bd818a3568c9ef
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 14:27:18 -0500

    Update 2-safe_print_list_integers.py - fix empty list with IndexError exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print(end="")
         except TypeError:
             print(end="")
+        except IndexError:
+            print(end="")
         i += 1
     if x > 0:
         print()
