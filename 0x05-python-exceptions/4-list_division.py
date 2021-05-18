c005fcc5c1cd5ac59073977574200cf56e892d84
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 19:52:52 -0500

    Update 4-list_division.py - Simplify some processes at the logical

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    re_list = []
     i = 0
-    if len(my_list_1) == 0 or len(my_list_2) == 0:
-        return []
-    mn_list = max(len(my_list_1), len(my_list_2))
-    re_list = [0] * mn_list
-    while i < mn_list:
+    div = 0
+    while i < list_length:
         try:
-            re_list[i] = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            re_list[i] = 0
+            div = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("Out of range")
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            re_list[i] = 0
-        except IndexError:
-            print("out of range")
-            re_list[i] = 0
+            div = 0
+        except TypeError:
+            print("wrong type")
+            div = 0
         finally:
+            re_list.append(div)
             i += 1
     return re_list
+
