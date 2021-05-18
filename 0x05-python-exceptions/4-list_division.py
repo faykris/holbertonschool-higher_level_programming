a249b3a7a0679a4311cfc2ae807cb6d60a7ab63c
Cristian Pinzón <faykris28@gmail.com>
2021-05-17 20:25:15 -0500

    Update 4-list_division.py - Adding parenthesis for division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     div = 0
     for i in range(list_length):
         try:
-            div = my_list_1[i] / my_list_2[i]
+            div = (my_list_1[i] / my_list_2[i])
         except IndexError:
             print("Out of range")
             div = 0
