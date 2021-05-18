1f58fd224cbece68da2fd46b38d59df36650cbfe
Cristian Pinzón <faykris28@gmail.com>
2021-05-18 16:09:37 -0500

    Add 2-square.py file - try with the checker

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""0. My first square
+"""1. Square with size
     Write a class Square that defines a square by: (based on 0-square.py)
     - Private instance attribute: size
     - Instantiation with size (no type/value verification)
