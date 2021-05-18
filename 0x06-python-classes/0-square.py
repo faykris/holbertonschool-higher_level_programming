b9426c103e488c039aeed0682f81cc8e7dd8b9e2
Cristian Pinzón <faykris28@gmail.com>
2021-05-18 13:43:14 -0500

    Add README.d and 0-square.py files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""0. My first square
+    Write an empty class Square that defines a square:
+    - You are not allowed to import any module
+"""
+
+
+class Square:
+    """Square
+    Only have a pass instruction, not args, not functions
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+# 0x06. Python - Classes and Objects
+This directory contains the Class and objects tasks files
