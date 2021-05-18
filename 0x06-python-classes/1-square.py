21e0f99e17cdb58b4ff01c40728f505347acc2e4
Cristian Pinzón <faykris28@gmail.com>
2021-05-18 15:37:29 -0500

    Add 1-square.py file - try with the checker

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0. My first square
+    Write a class Square that defines a square by: (based on 0-square.py)
+    - Private instance attribute: size
+    - Instantiation with size (no type/value verification)
+    - You are not allowed to import any module
+"""
+
+
+class Square:
+    """Square
+    Init function to instance square size
+    """
+    def __init__(self, _Square__size):
+        self._Square__size = _Square__size
