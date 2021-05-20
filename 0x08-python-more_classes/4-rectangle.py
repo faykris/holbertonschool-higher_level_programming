6fcd1db5e6739e6e2013ece532812c8a9ce83e69
Cristian Pinzón <faykris28@gmail.com>
2021-05-20 18:27:31 -0500

    fix 1-rectangle.py - add <= 0 condition in a setter

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
         """
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
