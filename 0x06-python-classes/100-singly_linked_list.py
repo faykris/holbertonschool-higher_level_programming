7e2b669b7b4f6d14fe3e6408740b54776235738a
Cristian Pinzón <faykris28@gmail.com>
2021-05-19 19:59:06 -0500

    Add 100-singly_linked_list.py file - add logical of objets

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,60 +1,99 @@
-"""7 - Singly linked list
-    Write a class Node that defines a node of a singly linked
+#!/usr/bin/python3
+"""7. Singly linked list
+    Write a class Node that defines a node of a singly linked list.
 """
 
 
 class Node:
-    """Node
-        Class when i can work with a node of a singly linked list
+    """Class Node
+        Creation of a node to add a singly linked list
     """
     def __init__(self, data, next_node=None):
-        """Init method
-        Args:
-            data:
-            next_node:
+        """ Init method
+            Args:
+                data: number to add
+                next_node: pointer to next node
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
-        """ data getter:
-            Take the information
+        """data getter
+            Returns:
+                The number to store ina node
         """
-        pass
+        return self.__data
 
     @data.setter
-    def data(self, value):
-        """ data setter:
-            Validate the information
+    def data(self, data):
+        """data getter
+             Returns:
+                instance of data
         """
-        pass
+        if type(data) != int:
+            raise TypeError("data must be an integer")
+        self.__data = data
 
     @property
     def next_node(self):
-        """ data getter:
-            Take the information
+        """data getter
+            Returns:
+                The pointer of a next node
         """
-        pass
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """ next_node setter:
-            Validate the information
+        """data getter
+            Returns:
+                instance of the next node
         """
-        pass
+        if type(value) != Node and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """SinglyLinkedList
-        class that define a singly linked list
-    """
-
     def __init__(self):
         """Init method
-            No arguments yet
+            init head with value None
         """
-        pass
+        self.__head = None
 
     def sorted_insert(self, value):
-        pass
+        """sorted_insert method
+            Args:
+                value: Node to be inserted
+        """
+        if self.__head is None:
+            new_node = Node(value)
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node = Node(value, self.__head)
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while temp.next_node is not None:
+                if temp.next_node.data > value:
+                    new_node = Node(value, temp.next_node)
+                    temp.next_node = new_node
+                    return
+                temp = temp.next_node
+            if temp.next_node is None:
+                new_node = Node(value)
+                temp.next_node = new_node
+
+    def __str__(self):
+        """str method
+            build a message with the sorted nodes
+        """
+        temp = self.__head
+        msg = ""
+        while temp is not None:
+            if temp.next_node is None:
+                msg += str(temp.data)
+            else:
+                msg += str(temp.data) + "\n"
+            temp = temp.next_node
+        return msg
