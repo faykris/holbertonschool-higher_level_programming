625539dd3d5027b079ce49fe9f86ad9426fbb2d9
Cristian Pinzón <faykris28@gmail.com>
2021-05-31 22:53:18 -0500

    fix 8-class_to_json.py - another way without import json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""----- 6. Create object from a JSON file -----
+"""----- 8. Class to JSON -----
     Write a function that returns the dictionary description
     with simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object:
 """
-import json
 
 
 def class_to_json(obj):
     """----- class_to_json function -----
-    creates an Object from a “JSON file”
+    function that returns the dictionary description
     Args:
         obj: file for store the representation
     Returns:
         Object created.
     """
-    return json.dumps(obj.__dict__)
+    return dict((key, value)for (key, value) in obj.__dict__.items())
