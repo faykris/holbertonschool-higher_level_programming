eeec4abb058695923dfc0fca145c5634688ac19a
Cristian Pinzón <faykris28@gmail.com>
2021-06-02 22:41:02 -0500

    Add 9-rectangle.py - test with the checker

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" 8. Rectangle
+    Write a class Rectangle that inherits from BaseGeometry
+    (7-base_geometry.py).
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        constructor of Rectangle class
+        Args:
+             width: value of width
+             height: value of height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        """ area method
+        """
+        return self._width * self._height
+
+    def __str__(self):
+        """str method
+        """
+        return "[Rectangle] {:d}/{:d}".format(self._width, self._height)
