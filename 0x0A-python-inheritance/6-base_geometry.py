5de5e0ba07547ab780d93769260d43167652f5ed
Cristian Pinzón <faykris28@gmail.com>
2021-06-02 19:49:58 -0500

    Add 6-base_geometry.py - test with the checker

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 6. Improve Geometry
+    Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry Class
+        with are method that shows an exception
+    """
+
+    def area(self):
+        """ area method
+            Returns:
+                Exception with message
+        """
+        raise Exception("area() is not implemented")
