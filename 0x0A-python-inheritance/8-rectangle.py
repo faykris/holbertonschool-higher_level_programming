74b136373ef6fa451534d83f3c23c7469c3dbb70
Cristian Pinzón <faykris28@gmail.com>
2021-06-02 22:28:09 -0500

    Fix 8-rectangle.py - fix pep8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ 8. Rectangle
-    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+    Write a class Rectangle that inherits from BaseGeometry
+    (7-base_geometry.py).
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ Class Rectangle
-        
     """
 
     def __init__(self, width, height):
