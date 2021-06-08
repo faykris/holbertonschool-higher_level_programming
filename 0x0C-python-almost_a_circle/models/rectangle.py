011a8ef7dfafb018607910cf32b45eab635350a0
Cristian Pinzón <faykris28@gmail.com>
2021-06-08 15:34:36 -0500

    fix rectangle.py - trying changing with underscore the init values

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,10 +99,10 @@ class Rectangle(Base):
                 y: displacing in y
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self._width = width
+        self._height = height
+        self._x = x
+        self._y = y
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
