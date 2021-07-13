bd23f0ef43128f4ce0d85bc53858c09ab9b59a4a
faykris <faykris28@gmail.com>
2021-07-13 16:56:04 -0500

    add 6-states.sql - test with Holberton checker

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- 6. States table
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) DEFAULT NULL,
+	UNIQUE (id),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;	
