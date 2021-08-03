a70f7ec90898a204746b74cde6d651383c3acb50
faykris <faykris28@gmail.com>
2021-08-03 18:58:26 -0500

    update 6-square.js - change require by 5-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     let i = 0;
     let j = 0;
