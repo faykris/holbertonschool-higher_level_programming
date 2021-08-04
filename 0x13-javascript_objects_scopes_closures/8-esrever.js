0e7dd7c9990b6b6b6bc56cbb18b04844fda3ab9e
faykris <faykris28@gmail.com>
2021-08-03 21:05:44 -0500

    add 8-esrever.js - test with Holberton checker

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const rev = [];
+  let i = list.length - 1;
+
+  while (i >= 0) {
+    rev.push(list[i]);
+    i--;
+  }
+  return rev;
+};
