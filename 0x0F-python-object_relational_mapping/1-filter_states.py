00ed4c0f23ed588cba088d3064e6d5c17c6670e3
faykris <faykris28@gmail.com>
2021-08-07 18:15:59 -0500

    update 1-filter_states.py - add BINARY statement in query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,9 @@ import MySQLdb
 def realize_query(user, password, database):
     """function that connect with mysql and runs a query"""
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         password=password, database=database,
-                         charset="utf8")
+                         password=password, database=database)
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    sql = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
