b7010a64b2c0f0912e2f7423e3fb67cb851f6314
faykris <faykris28@gmail.com>
2021-08-07 17:58:28 -0500

    update model_state.py - erase session lines

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,11 +5,11 @@
 from sys import argv
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
 
-port = 3306
-connection = """mysql+mysqldb://{}:{}@localhost:{}/{}
-             """.format(argv[1], argv[2], port, argv[3])
+
+connection = "mysql+mysqldb://{}:{}@{}:{}/{}"\
+            .format(argv[1], argv[2], "localhost", 3306, argv[3])
+
 engine = create_engine(connection)
 Base = declarative_base()
 
@@ -20,7 +20,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-session = Session(engine)
-session.close()
