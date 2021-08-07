6311c2c3a3315a58c25e00c454824f082b3c45cd
faykris <faykris28@gmail.com>
2021-08-07 09:50:08 -0500

    update 1 and 2 tasks files - trying adding charset in mysql connect

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3])
+                         password=sys.argv[2], database=sys.argv[3],
+                         charset="utf8")
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,11 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3])
+                         password=sys.argv[2], database=sys.argv[3],
+                         charset="utf8")
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name = '{}'
-                   ORDER BY states.id ASC;""".format(sys.argv[4]))
+                   ORDER BY states.id ASC""".format(sys.argv[4]))
     results = cursor.fetchall()
     for row in results:
         print(row)
