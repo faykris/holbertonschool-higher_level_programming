c42ef0ca02223c1a85e571b9416a88c7e8534f8b
faykris <faykris28@gmail.com>
2021-08-07 18:20:41 -0500

    update 2-my_filter_states.py - erase session.close()

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,15 +9,13 @@ import MySQLdb
 def realize_query(user, password, database, state):
     """function that connect with mysql and runs a query"""
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         password=password, database=database,
-                         charset="utf8")
+                         password=password, database=database)
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name = '{}'
-                      ORDER BY states.id ASC;""".format(state))
+                      ORDER BY states.id ASC""".format(state))
     results = cursor.fetchall()
     for row in results:
         print(row)
-    cursor.close()
 
 
 if __name__ == "__main__":
