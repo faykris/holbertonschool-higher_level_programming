803b7fea76bbb56ca6c3a47a09c3642ffd685c5b
faykris <faykris28@gmail.com>
2021-08-07 09:30:25 -0500

    update 2-my_filter_states.py - try giving directly query to be executed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
-    sql = """SELECT * FROM states WHERE name = '{}'
-             ORDER BY states.id ASC""".format(sys.argv[4])
-    cursor.execute(sql)
+    cursor.execute("""SELECT * FROM states WHERE name = '{}'
+                   ORDER BY states.id ASC""".format(sys.argv[4]))
     results = cursor.fetchall()
     for row in results:
         print(row)
