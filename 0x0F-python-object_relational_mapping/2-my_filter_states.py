59e0e110a114c001b2cf888f46edf1daca19b224
faykris <faykris28@gmail.com>
2021-08-07 09:07:02 -0500

    update 2-my_filter_states.py - fix pep8 and erase cursor.close()

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
-    sql = """SELECT * FROM states WHERE name = '{}' 
+    sql = """SELECT * FROM states WHERE name = '{}'
              ORDER BY states.id ASC""".format(sys.argv[4])
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
         print(row)
-    cursor.close()
     db.close()
