93fc8aec4b7a79c0d3d46b2241e29f51f1146267
faykris <faykris28@gmail.com>
2021-08-07 09:05:27 -0500

    update 2-my_filter_states.py - change query in .format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name = '" + sys.argv[4] +\
-          "' ORDER BY id ASC"
+    sql = """SELECT * FROM states WHERE name = '{}' 
+             ORDER BY states.id ASC""".format(sys.argv[4])
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
