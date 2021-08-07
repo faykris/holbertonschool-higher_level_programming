221f0cc1559b02404a5e81e2a5e98469fece7224
faykris <faykris28@gmail.com>
2021-08-07 08:50:22 -0500

    update 1 to 4 tasks files - change args by kwargs in connect and change print in for loop

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,13 +6,13 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
     sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
-        id = row[0]
-        name = row[1]
-        print("({}: '{}')".format(id, name))
+        print(row)
+    cursor.close()
     db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
     sql = """SELECT cities.id, cities.name, states.name FROM states
              INNER JOIN cities ON states.id = cities.state_id
@@ -14,8 +15,5 @@ if __name__ == "__main__":
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
-        id = row[0]
-        city = row[1]
-        state = row[2]
-        print("({}: '{}', '{}')".format(id, city, state))
+        print(row)
     db.close()
