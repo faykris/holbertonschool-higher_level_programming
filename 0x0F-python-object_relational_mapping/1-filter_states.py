7314862b0e52f43658b8bd65d175e7d48e7573d6
faykris <faykris28@gmail.com>
2021-08-07 09:54:40 -0500

    update 1 and 2 tasks files - trying adding values with variables in msql connect

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,8 +6,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3],
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         password=password, database=database,
                          charset="utf8")
     cursor = db.cursor()
     sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3],
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         password=password, database=database,
                          charset="utf8")
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name = '{}'
