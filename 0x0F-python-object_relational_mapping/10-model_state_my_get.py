0df21faf888e7f1f12fa2bbf37c0fc69bfa17324
faykris <faykris28@gmail.com>
2021-08-07 21:49:30 -0500

    update 10-model_state_my_get.py - trying with adjustments by the task 6

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,14 +2,19 @@
 """
 10. Get a state
 """
-import sys
-from model_state import Base, State, engine, Session
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
     i = 0
-    for state in session.query(State).filter(State.name == sys.argv[4]):
+    for state in session.query(State).filter(State.name == argv[4]):
         i += 1
     if i != 0:
         print("{}".format(i))
