425cf1a445d500c529538ca2d4bc8dccfdedcf8c
faykris <faykris28@gmail.com>
2021-08-07 21:42:51 -0500

    update 8-model_state_fetch_first.py - trying with adjustments by the task 6

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,9 +2,15 @@
 """
 8. First state
 """
-from model_state import Base, State, engine, Session
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
     state = session.query(State).first()
