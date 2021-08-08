591e0f9f0aa355ba4d8749c3f0ce867c3d071812
faykris <faykris28@gmail.com>
2021-08-07 23:43:27 -0500

    add 11-model_state_insert.py - test with Holberton checker

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+10. Get a state
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine, insert)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    new = State(name='Louisiana')
+    session.add(new)
+    session.commit()
+    print(new.id)
+    session.close()
