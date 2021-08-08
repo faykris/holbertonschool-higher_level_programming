b42b3f6d1415dcb6cdcd8bdf50f69c415b67d46c
faykris <faykris28@gmail.com>
2021-08-08 14:13:09 -0500

    add 14-model_city_fetch_by_state.py and mode_city.py - test with Holberton checker

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+10. Get a state
+"""
+from sys import argv
+from model_state import State
+from model_city import Base, City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for row in session.query(State.name, City.id, City.name)\
+            .join(City, State.id == City.state_id).order_by(State.id).all():
+        print("{}: ({}) {}".format(row[0], row[1], row[2]))
+    session.close()
