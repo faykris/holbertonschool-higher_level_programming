24e20f72c4b328e81cc4189fb587b3ea1869be90
faykris <faykris28@gmail.com>
2021-08-20 14:44:11 -0500

    update 1-hbtn_header.py - add sys.argv[1] and change logical by .get

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ task 0 module """
 import urllib.request
+import sys
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    info_dict = response.info().__dict__
-    for h_list in info_dict.values():
-        if type(h_list) is list and len(h_list) != 0:
-            for h_tu in h_list:
-                if h_tu[0] == 'X-Request-Id':
-                    print(h_tu[1])
+with urllib.request.urlopen(sys.argv[1]) as response:
+    print(response.info().get('X-Request-Id'))
