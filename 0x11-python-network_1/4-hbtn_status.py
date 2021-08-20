c893eaa90ef9d7c838dbb5a78d3521b8488f0c81
faykris <faykris28@gmail.com>
2021-08-20 16:26:56 -0500

    update 4-hbtn_status.py - change by import request logical

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ task 0 module """
-import urllib.request
+
+import requests
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html.decode('utf-8'))))
-        print("\t- content: {}".format(html.decode('utf-8')))
+    r = requests.get('https://intranet.hbtn.io/status')
+    content = r.content.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
