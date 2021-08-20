1b2c1c48cced85556ebe1cac47a135ded2a19630
faykris <faykris28@gmail.com>
2021-08-20 18:49:15 -0500

    add 8-json_api.py - test with Holberton checker

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" task 7 module """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        user = {'q': sys.argv[1]}
+    except IndexError:
+        user = {'q': ""}
+    r = requests.post(url, user)
+
+    if len(r.json()) == 0:
+        print("No result")
+    elif r.json().get('id') is None or r.json().get('name') is None:
+        print("Not a valid JSON")
+    else:
+        print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
