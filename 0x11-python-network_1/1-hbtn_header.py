200233e647a595df295a270031aa66552f641e15
faykris <faykris28@gmail.com>
2021-08-20 14:55:34 -0500

    update 1-hbtn_header.py - add if not imported condition

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,6 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    info_dict = response.info().__dict__
-    for h_list in info_dict.values():
-        if type(h_list) is list and len(h_list) != 0:
-            for h_tu in h_list:
-                if h_tu[0] == 'X-Request-Id':
-                    print(h_tu[1])
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
