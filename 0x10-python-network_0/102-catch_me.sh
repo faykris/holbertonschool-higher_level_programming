3eed95a3298a81f5f4ed8c606fe043dfe7395d99
faykris <faykris28@gmail.com>
2021-08-22 20:55:39 -0500

    update 102-catch_me.sh - trying with short flags

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # request that causes the server to respond with message containing You got me!
-curl --silent -L "0.0.0.0:5000/catch_me" --request PUT --header "Origin:HolbertonSchool" --data "user_id=98"
+curl -sL "0.0.0.0:5000/catch_me" -X PUT -H "Origin:HolbertonSchool" -d "user_id=98"
