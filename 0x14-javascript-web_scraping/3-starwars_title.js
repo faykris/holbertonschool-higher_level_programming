b5914f127ca0833360185ec45522a13af09d1033
faykris <faykris28@gmail.com>
2021-09-14 17:56:03 -0500

    add 3-starwars_title.js - test with Holberton checker

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, response, body) => {
+  if (error) return console.log(error);
+  console.log(JSON.parse(body).title);
+});
